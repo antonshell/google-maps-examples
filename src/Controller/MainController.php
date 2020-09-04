52c76a14ce3123b149e289bbd2ff85a721ec3b8b
antonshell <antonshell@yandex.ru>
2020-09-04 14:23:03 +0300

    Implemented index action

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+}
